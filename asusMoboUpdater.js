d852eabe295f94a910684237e74ce2a78b27bc22
Dave Richer <dave@ir0ny.com>
2018-01-27 14:11:33 -0500

    - add docbocks and other things to make WebStorm stop chirping - change download path to reflect file path in URL - change MQTT dispatch output to stringified JSON

--- asusMoboUpdater.js
+++ asusMoboUpdater.js
@@ -1,18 +1,45 @@
+/** @namespace biosResults.Status */
+/** @namespace biosResults.Result */
+/** @namespace biosResults.Obj */
+/** @namespace biosResults.Onj.Files */
+/** @namespace biosResults.Files.FileSize */
+/** @namespace biosResults.Files.Version */
+/** @namespace biosResults.Files.Description */
+/** @namespace biosResults.Files.DownloadUrl */
+/** @namespace biosResults.Files.DownloadUrl.Global */
+/** @namespace lastBios.Title */
+/** @namespace lastBios.ReleaseDate */
+
 const c = require('chalk');
 const fs = require('fs');
 const rp = require('request-promise-native');
-const url = require('url');
 const strip = require('striptags');
 
-// Formatter
-const f = (key, value) => console.log(c.cyan(key || ''), c.blue(value || ''));
+/**
+ * Console output formatter
+ * @param key
+ * @param value
+ */
+const formattedConsole = (key, value) => console.log(c.cyan(key || ''), c.blue(value || ''));
 
-// Parse Desc as JS Object
-const descToObject = (desc) => strip(desc).replace(/m\.2/gi, 'M2').split('.').map(x => x.trim()).filter(x => x !== '');
+/**
+ * Split a API Description into a Array
+ * @param desc
+ * @return {string[]}
+ */
+const descToObject = (desc) => strip(desc)
+    .replace(/m\.2/gi, 'M2')
+    .split('.')
+    .map(x => x.trim())
+    .filter(x => x !== '');
 
-// Validate the API State
+/**
+ * Validate API Result Object
+ * @param biosResults
+ * @return {boolean}
+ */
 const validApiState = (biosResults) =>
-    biosResults.Status === 'SUCCESS' &&
+    biosResults.Status === /** @type {boolean} */'SUCCESS' &&
     biosResults.Result &&
     biosResults.Result.Obj &&
     biosResults.Result.Obj[0] &&
@@ -24,13 +51,21 @@ const validApiState = (biosResults) =>
     biosResults.Result.Obj[0].Files[0].DownloadUrl &&
     biosResults.Result.Obj[0].Files[0].DownloadUrl.Global;
 
-// Is Valid Motherboard State
+/**
+ * Validation Motherboard Object
+ * @param mobo
+ * @return {boolean}
+ */
 const validMoboState = (mobo) => mobo &&
     mobo.currentVersion &&
     mobo.name &&
     mobo.apiEndPoint;
 
-// Is Valid Configuration
+/**
+ *  Validate Configuration Object
+ * @param config
+ * @return {boolean}
+ */
 const validConfig = (config) =>
     config.downloadPath &&
     config.mqttTitle &&
@@ -40,42 +75,62 @@ const validConfig = (config) =>
     config.broker.username &&
     config.broker.password;
 
+/**
+ * Check and Download for updated BIOS's from Asus
+ * @param config
+ * @param mqttClient
+ * @return {Promise<void>}
+ */
 const asusMoboUpdater = async (config, mqttClient) => {
     // Validate Config
     if (!validConfig(config)) {
         throw new Error('Configuration Object is malformed');
     }
 
-    // Get a Download file path
+    /**
+     * Generate potential file path
+     * @param mobo
+     * @param lastBios
+     * @return {string}
+     */
     const getFilePath = (mobo, lastBios) => {
-//        `${config.downloadPath}/${mobo.name}-${lastBios.Version}.zip`
-        const fileUrl = url.parse(lastBios.DownloadUrl.Global);
-        const fileResults  = fileUrl.pathname.split('/');
+        const fileResults = lastBios.DownloadUrl.Global.split('/');
         return `${config.downloadPath}/${fileResults[fileResults.length - 1]}`;
     };
 
-    // Alert of New Bios
+    /**
+     * Console Alert
+     * @param mobo
+     * @param lastBios
+     */
     const newBiosConsoleAlert = (mobo, lastBios) => {
         console.log(c.red(`Your current BIOS for ${mobo.name} ${mobo.currentVersion}, is not up to date`));
-
-        f('Release Date', lastBios.ReleaseDate);
-        f('Title', lastBios.Title);
-        f('Description');
-        f('URL', lastBios.DownloadUrl.Global);
+        formattedConsole('Release Date', lastBios.ReleaseDate);
+        formattedConsole('Title', lastBios.Title);
+        formattedConsole('Description');
+        formattedConsole('URL', lastBios.DownloadUrl.Global);
 
         descToObject(lastBios.Description).forEach(note => console.log(c.yellow(`- ${note}`)));
 
         if (downloadExists(mobo, lastBios)) {
-            f('Downloaded', lastBios.filePath);
+            formattedConsole('Downloaded', lastBios.filePath);
         }
     };
 
-    // Download file Exists
-    const downloadExists = (mobo, lastBios) => {
-        return fs.existsSync(lastBios.filePath);
-    };
+    /**
+     * Check to see if a BIOS was already downloaded
+     * @param mobo
+     * @param lastBios
+     * @return {boolean}
+     */
+    const downloadExists = (mobo, lastBios) => fs.existsSync(lastBios.filePath);
 
-    // Download New Bioses
+    /**
+     * Download New BIOS
+     * @param mobo
+     * @param lastBios
+     * @return {Promise<boolean>}
+     */
     const newBiosDownloadCheck = async (mobo, lastBios) => {
         // If the File does not already exist
         if (!downloadExists(mobo, lastBios)) {
@@ -88,29 +143,38 @@ const asusMoboUpdater = async (config, mqttClient) => {
             });
 
             fs.writeFileSync(lastBios.filePath, Buffer.from(fileRes, 'utf8'));
+
             console.log('Downloaded');
-            return;
+            return true;
         }
 
         console.log(c.yellow('Already Downloaded'));
+        return false;
     };
 
     // MQTT alert
+    /**
+     * Send a MQTT Alert
+     * @param mobo
+     * @param lastBios
+     * @return {Promise<*|MqttClient>}
+     */
     const newBiosMqttAlert = async (mobo, lastBios) =>
         mqttClient.publish(
             config.mqttTitle,
             JSON.stringify({
                 mobo,
                 lastBios: Object.assign({}, lastBios, {
-                    description: strip(lastBios.Description).replace(/  +/g, ' ')
+                    HTMLDescription: lastBios.Description,
+                    Description: strip(lastBios.Description).replace(/ {2}/g, ' ')
                 })
             }),
         );
 
-    // Iterate over the mobos
+    // Iterate over the motherboards
     for (const mobo of config.mobos) {
         // Verify mobo state
-        if (!validMoboState) {
+        if (!validMoboState(mobo)) {
             console.error('Malformed mobo entry');
             continue;
         }
@@ -135,7 +199,17 @@ const asusMoboUpdater = async (config, mqttClient) => {
         }
         // BIOS is not up to date
         newBiosConsoleAlert(mobo, lastBios);
-        await newBiosDownloadCheck(mobo, lastBios);
+        try {
+            await newBiosDownloadCheck(mobo, lastBios);
+        } catch (err) {
+            console.log('Something went wrong fetching a BIOS');
+            console.dir({
+                message: err.message,
+                stack: err.stack,
+                mobo,
+                lastBios
+            });
+        }
         // If mqttClient is available, broadcast
         if (mqttClient) await newBiosMqttAlert(mobo, lastBios);
     }
